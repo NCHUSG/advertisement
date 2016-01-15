8d67266dd59c20968a7986381ca2a196c220a8ed
david <davidtnfsh@gmail.com>
2016-01-15 16:48:17 +0800

    make js to script link

---
+++ ad.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  $("#fancybox-manual-b").click(function() {
+    $.fancybox.open({
+      href : '../advertisement/index.html',//在這邊放絕對網址會讓圖片爆掉,我還沒解決，所以讓他用相對路徑直接去連到廣告的頁面../advertisement/index.html
+      type : 'iframe',
+      padding : 5
+    });
+  });
+  $("#fancybox-manual-b").click();
+});              
