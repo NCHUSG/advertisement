538fb2e09c8c1c0c16705dfa1df33d2db7463196
david <davidtnfsh@gmail.com>
2016-01-16 13:41:54 +0800

    看起來相對連結再伺服器上有問題，我改絕對路徑

--- ad.js
+++ ad.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   $("#fancybox-manual-b").click(function() {
     $.fancybox.open({
-      href : '../advertisement/index.html',//在這邊放絕對網址會讓圖片爆掉,我還沒解決，所以讓他用相對路徑直接去連到廣告的頁面../advertisement/index.html
+      href : 'http://ad.nchusg.org/index.html',//在這邊放絕對網址會讓圖片爆掉,我還沒解決，所以讓他用相對路徑直接去連到廣告的頁面../advertisement/index.html
       type : 'iframe',
       padding : 5
     });
