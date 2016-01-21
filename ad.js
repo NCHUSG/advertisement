2d02274501a9eaae56fc4a108313393b2d7939fa
josh <j9963232q@gmail.com>
2016-01-21 22:02:10 +0800

    圖片大小調整測試

--- ad.js
+++ ad.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
     $.fancybox.open({
       href : 'http://ad.nchusg.org/index.html',//在這邊放絕對網址會讓圖片爆掉,我還沒解決，所以讓他用相對路徑直接去連到廣告的頁面../advertisement/index.html
       type : 'iframe',
-      padding : 5
+      padding : 5,
+      autoSize: false
     });
   });
   $("#fancybox-manual-b").click();
