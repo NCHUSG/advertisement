429c9c996faa9c4d37f0a99af4ca16077e4bd087
josh <j9963232q@gmail.com>
2016-01-22 22:21:36 +0800

    廣告修復

--- ad.js
+++ ad.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   $("#fancybox-manual-b").click(function() {
-    $.fancybox({autoSize: false}).open({
+    $.fancybox().open({
       href : 'http://ad.nchusg.org/index.html',//在這邊放絕對網址會讓圖片爆掉,我還沒解決，所以讓他用相對路徑直接去連到廣告的頁面../advertisement/index.html
       type : 'iframe',
       padding : 5
